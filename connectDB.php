7dcb8ba2c9f5a4a645d30de275f3d7674f33d496
BrandenAlexanderLiaw <102653489+BrandenAlexanderLiaw@users.noreply.github.com>
2022-05-11 13:39:19 +0800

    Update connectDB.php

--- connectDB.php
+++ connectDB.php
@@ -1,7 +1,7 @@
 <?php
 
 //Connect to database named "pineconecc"
-    $conn = mysqli_connect('localhost', 'root', '', 'pineconecc');
+    $conn = mysqli_connect('localhost', 'root', '', 'pineconecc') or die ("Connection error");
     
     if(!$conn){
         echo'connection error: ' . mysqli_connect_error();
